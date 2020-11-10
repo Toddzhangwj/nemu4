2d18c1ed329f2ec104aac3837af77948a56f72aa
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-10 14:10:27 +0000

    >  compile NEMU 3018216092zd simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:10:27 up 1 day,  2:12,  1 user,  load average: 2.20, 2.13, 1.60 e12cf852f25c620897ad40ae6930aab4379143f3

--- nemu/src/cpu/exec/intr/intr.c
+++ nemu/src/cpu/exec/intr/intr.c
@@ -13,5 +13,10 @@ void raise_intr(uint8_t NO) {
 	lnaddr_t addr = cpu.idtr.base_addr+NO*8;
 	gate.first=lnaddr_read(addr,4);
 	gate.second=lnaddr_read(addr+4,4);
-	
+	push(cpu.eflags);
+	if(cpu.cr0.protect_enable==0) {
+		cpu.IF=0;
+		cpu.TF=0;	
+	}
+	push(cpu.cs.selector);
 }
