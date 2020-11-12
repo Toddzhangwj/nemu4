a85b2f9bfb4de4e443548c3da0b630e225f2afab
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-12 02:56:53 +0000

    >  compile NEMU 3018216092zd simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:56:53 up 1 day, 11:40,  1 user,  load average: 3.39, 3.06, 3.08 8b81eee9e5af5f69a4d3cb76c6c7b83b28165a1e

--- nemu/src/cpu/exec/intr/int.c
+++ nemu/src/cpu/exec/intr/int.c
@@ -1,3 +1,12 @@
 #include "cpu/exec/helper.h"
 #include "nemu.h"
 void raise_intr(uint8_t);
+
+make_helper(Int) {
+	int no = instr_fetch(eip+1,1);
+	cpu.eip+=2;
+	print_asm("int %x",no);
+	raise_intr(no);
+	return 0;
+	
+}
