b540fbe0cfe262a3d3f52be6c835ab925b5efdf0
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-20 09:47:31 +0000

    >  compile NEMU 3018216092zd simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:47:31 up 9 days, 18:30,  1 user,  load average: 1.58, 1.37, 1.28 a441420f61d4714ae368a05a5158156ec7b2f86f

---
+++ nemu/src/cpu/exec/logic/bt-template.h
@@ -0,0 +1,11 @@
+#include "cpu/exec/template-start.h"
+
+#define instr bt
+
+static void do_execute() {
+	cpu.CF = (op_dest->val>>op_src->val)&1;
+	print_asm_template2();
+}
+
+
+#include "cpu/exec/template-end.h"
