ec51105a240df95b2542bb95ab08509502b52d43
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-20 09:33:52 +0000

    >  compile NEMU 3018216092zd simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:33:52 up 9 days, 18:17,  1 user,  load average: 1.72, 1.47, 1.22 5793dda5eecc4223fc5732ab9b722284e260c10

---
+++ nemu/src/cpu/exec/data-mov/cmovcc-template.h
@@ -0,0 +1,11 @@
+#include "cpu/exec/template-start.h"
+
+#define make_cmovcc_helper(cc) \
+	make_helper(concat4(cmov, cc, _, SUFFIX)) { \
+		int len = concat(decode_rm2r_, SUFFIX)(eip + 1); \
+		(concat(check_cc_, cc)() ? OPERAND_W(op_dest, op_src->val) : 0 ); \
+		print_asm(str(concat(cmov, cc)) str(SUFFIX) " %s,%s", op_src->str, op_dest->str); \
+		return len + 1; \
+	}
+
+#include "cpu/exec/template-end.h"
