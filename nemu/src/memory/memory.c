327bf9142074fd9de8cc38d66814ec54b9a0425b
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-20 12:15:36 +0000

    >  compile NEMU 3018216092zd simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:15:36 up 9 days, 20:58,  1 user,  load average: 0.94, 1.13, 1.12 b1a69712af4772fea80ffff3e940b24325fdfa61

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -20,18 +20,21 @@ lnaddr_t seg_translate(swaddr_t addr,size_t len,uint8_t sreg) {
 
 hwaddr_t page_translate(lnaddr_t addr,size_t len) {
 	if(cpu.cr0.paging==1&&cpu.cr0.protect_enable==1) {
-		hwaddr_t ans = tlb_read(addr&0xfffff000);
-		if(ans!=-1) return (ans<<12) + (addr&0xfff);
+		//hwaddr_t ans = tlb_read(addr&0xfffff000);
+		//if(ans!=-1) return (ans<<12) + (addr&0xfff);
 		uint32_t dir = addr >> 22; 
 		uint32_t page = (addr>>12)&0x3ff;
 		uint32_t offset = addr&0xfff;
 		Page_entry dir_1,page_1;
 		dir_1.val = hwaddr_read((cpu.cr3.page_directory_base<<12)+(dir<<2),4);
-		Assert(dir_1.p,"Invalid page");
+		Assert(dir_1.p,"pagevalue = %x eip = %x", dir_1.val,cpu.eip);
 		page_1.val = hwaddr_read((dir_1.base<<12)+(page<<2),4);
-		Assert(page_1.p,"Invalid page");
+		if(!page_1.p) {
+			printf("\n%x\n",addr);	
+		}
+		Assert(page_1.p,"page do not exist at %x", cpu.eip);
 		hwaddr_t hwaddr = (page_1.base<<12)+offset;
-		tlb_write(addr&0xfffff000,page_1.base);
+		//tlb_write(addr&0xfffff000,page_1.base);
 		//Assert((hwaddr&0xfff)+len==((hwaddr+len)&0xfff),"Fatal Error");
 		return hwaddr;
 	}
@@ -42,10 +45,10 @@ hwaddr_t page_translate(lnaddr_t addr,size_t len) {
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	int index = is_mmio(addr);
-	if (index !=-1) {
-		return mmio_read(addr,len,index);	
+	if (index >=0) {
+		return mmio_read(addr,len,index)&(~0u >> ((4 - len) << 3));	
 	}
-	int id = cache_read(addr);
+	/*int id = cache_read(addr);
 	uint32_t offset = addr&(CACHE_BLOCK_SIZE-1);
 	uint8_t tmp[2*BURST_LEN];
 	if(offset+len>CACHE_BLOCK_SIZE) {
@@ -58,16 +61,18 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	}
 	int zero=0;
 	uint32_t ans = unalign_rw(tmp+zero,4)&(~0u >> ((4 - len) << 3));
-	return ans;
+	return ans;*/
+	return dram_read(addr,len)&(~0u >> ((4 - len) << 3));
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
 	int index = is_mmio(addr);
-	if (index !=-1) {
+	if (index >=0) {
 		mmio_write(addr,len,data,index);
 		return ;
 	}
-	cache_write(addr, len, data);
+	//cache_write(addr, len, data);
+	dram_write(addr, len, data);
 }
 
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
@@ -95,4 +100,5 @@ void swaddr_write(swaddr_t addr, size_t len, uint32_t data,uint8_t sreg) {
 	lnaddr_t lnaddr = seg_translate(addr,len,sreg);
 	lnaddr_write(lnaddr, len, data);
 }
+
 
